de64e2f40bfac28cd91daea9485a0928907ac9a2
Ryan Cheung <ryanc@livefyre.com>
2013-01-29 12:25:37 -0800

    Use iframe youtube api to play one video at a time

--- views/YoutubeDeckFeedView.js
+++ views/YoutubeDeckFeedView.js
@@ -18,21 +18,69 @@ var YoutubeDeckFeedView = DeckFeedView.extend({
         var self = this;
         $('.deck-yt-play', $heading).click(function(e) {
             e.preventDefault();
-            console.warn('click play');
             self.embedVideo();
         });
         return $heading;
     },
     embedVideo: function() {
         var youtubeId = this.collection.youtubeId;
-        console.warn(youtubeId);
-        var iframe = document.createElement('iframe');
-        $(iframe).attr('width', 300);
-        $(iframe).attr('height', 169);
-        $(iframe).attr('frameborder', 0);
-        $(iframe).attr('allowfullscreen', '');
-        $(iframe).attr('src', 'http://www.youtube.com/embed/'+youtubeId+'/?autoplay=1&enablejsapi=1');
-        $('.deck-col-body', this.$heading).empty().append(iframe);
+        var $headingBody = $('.deck-col-body', this.$heading);
+        $headingBody.empty();
+
+        var tag = document.createElement('script');
+        tag.src = '//www.youtube.com/iframe_api';
+        //$headingBody.before(tag);
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        var self = this;
+
+        newPlayer($headingBody[0], {
+            videoId: youtubeId, 
+            width: 300,
+            height: 169,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        }); 
+
+        function newPlayer(el, opts) {
+            if (typeof YT == 'undefined') {
+                window._yt_players = [];
+                window.onYouTubeIframeAPIReady = function(){
+                    window._yt_players.push(getNewPlayer(el, opts));
+                }
+            } else {
+                window._yt_players.push(getNewPlayer(el, opts));
+            }
+
+            function getNewPlayer(el, opts) {
+                return new YT.Player(el, {
+                    width: opts.width,
+                    height: opts.height,
+                    videoId: opts.videoId,
+                    events: opts.events
+                });
+            }
+        }
+
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING) {
+                // trigger pause on other players
+                for (var i=0; i < window._yt_players.length; i++) {
+                    var player = window._yt_players[i];
+                    if (event.target.id == player.id) {
+                        continue;
+                    }
+                    player.pauseVideo();
+                }
+            }
+        }
     }
 });
 
