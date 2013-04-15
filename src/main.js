30a456690dcffdc6d01dff452be5525864a099aa
Ryan Cheung <ryanc@livefyre.com>
2013-04-15 15:34:21 -0700

    Update unit tests for deck view

--- src/main.js
+++ src/main.js
@@ -4,21 +4,39 @@ function($, Hub) {
         opts = opts || {};
         this.el = opts.el;
         this._collections = opts.collections || [];
-        this._streams = opts.streams || {};
+        this.streams = opts.streams || {};
 
-        if (!this._streams) {
+        var self = this;
+        if (!self.streams) {
             return;
         }
 
         // Setup container elements required for Deck view
-        $(this.el).addClass(opts.className || 'hub-DeckView');
+        $(self.el).addClass(opts.className || 'hub-DeckView');
         var $deckColumns = $(document.createElement('div')).addClass('deck-columns');
-        $(this.el).append($deckColumns);
+        $(self.el).append($deckColumns);
 
-        this._streams.forEach(function(stream) {
+        self.streams.forEach(function(stream, articleId) {
             // Create the containing element for a ListView
             var $deckCol = $(document.createElement('section')).addClass('deck-col');
             $deckColumns.append($deckCol);
+
+            // Create a heading if the collection has one specified
+            for (var i=0; i < self._collections.length; i++) {
+                if (self._collections[i].articleId != articleId) {
+                    continue;
+                }
+                var $deckColHeading = $(document.createElement('div')).addClass('deck-col-heading');
+                var $title = $(document.createElement('h2')).addClass('deck-col-title');
+                $title.html(self._collections[i].headingTitle);
+                $deckColHeading.append($title);
+                $deckCol.append($deckColHeading);
+
+                $headingContent = self.createHeadingContent(self._collections[i]);
+                if ($headingContent) {
+                    $deckCol.append($headingContent);
+                }
+            }
 
             // Create a deck column for a particular stream
             var $listViewContainer = $(document.createElement('div')).addClass('deck-col-container');
@@ -34,6 +52,23 @@ function($, Hub) {
                 el: columnEl
             });
         });
+    };
+
+    DeckView.prototype.createHeadingContent = function(collection) {
+        var $headingContent;
+        if ('youtubeId' in collection) {
+            //require('YoutubeColumnHeading');
+            //var heading = YoutubeColumnHeading();
+
+            // Create youtube player as heading content
+            //$headingContent = $(document.createDocumentFragment());
+            //var $youtubeThumb = $(document.createElement('img'));
+            //$youtubeThumb.attr('src', 'http://img.youtube.com/vi/' + collection['youtubeId'] + '/hqdefault.jpg');
+            //$youtubePlayer = $(document.createElement('div')).addClass('deck-yt-play');
+            //$headingContent.append($youtubeThumb);
+            //$headingContent.append($youtubePlayer);
+        }
+        return $headingContent
     };
 
     return DeckView;
