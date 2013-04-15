8ace9512de13d2c6b0803a415398220d2a70caf2
Ryan Cheung <ryanc@livefyre.com>
2013-04-15 10:48:29 -0700

    Refactor dom structure creation for deck view

--- src/main.js
+++ src/main.js
@@ -3,32 +3,30 @@ function($, Hub) {
     var DeckView = function(opts) {
         opts = opts || {};
         this.el = opts.el;
+        this._collections = opts.collections || [];
         this._streams = opts.streams || {};
 
         if (!this._streams) {
             return;
         }
 
-        $(this.el).addClass('hub-DeckView');
         // Setup container elements required for Deck view
-        var $deckColumns = $(document.createElement('div'));
-        $deckColumns.addClass('deck-columns').addClass('feeds');
+        $(this.el).addClass(opts.className || 'hub-DeckView');
+        var $deckColumns = $(document.createElement('div')).addClass('deck-columns');
         $(this.el).append($deckColumns);
 
         this._streams.forEach(function(stream) {
-            // Create the containing element for FeedView
-            var $deckCol = $(document.createElement('section'));
-            $deckCol.addClass('deck-col');
+            // Create the containing element for a ListView
+            var $deckCol = $(document.createElement('section')).addClass('deck-col');
             $deckColumns.append($deckCol);
 
-            $deckCol.append('<div class="deck-col-feedview-holder"></div>')
-            var $deckColScroll = $(document.createElement('div'));
-            $deckColScroll.addClass('deck-col-scroll');
-            $deckCol.find('.deck-col-feedview-holder').append($deckColScroll);
-
-            var $feed = $(document.createElement('div'));
-            $feed.addClass('feed');
+            // Create a deck column for a particular stream
+            var $listViewContainer = $(document.createElement('div')).addClass('deck-col-container');
+            var $deckColScroll = $(document.createElement('div')).addClass('deck-col-scroll');
+            var $feed = $(document.createElement('div')).addClass('deck-feed');
             $deckColScroll.append($feed);
+            $listViewContainer.append($deckColScroll);
+            $deckCol.append($listViewContainer);
 
             var columnEl = $feed[0];
             new Hub.Views.ListView({
